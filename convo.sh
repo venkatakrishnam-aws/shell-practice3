50fc882612d7a51f6c45eb03b610e8266d47b9e7
venkatakrishnam.aws <vk2860094@gmail.com>
2026-01-11 19:30:10 +0530

    This is a third update

--- convo.sh
+++ convo.sh
@@ -3,7 +3,7 @@
 $person1="Modi"
 $person2="Putin"
 
-echo "::Hello $person2 ! How are you?"
+echo "$person!::Hello $person2 ! How are you?"
 echo "$person2::Hey $person1 thanks for asking. I'm doing well!"
 echo "$person2::Hope you are doing well $person1?"
 
