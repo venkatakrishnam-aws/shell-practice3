5f73fd60d7e7a609682c112b283c0710902c5a4d
venkatakrishnam.aws <vk2860094@gmail.com>
2026-01-22 09:40:02 +0530

    this is my second push

--- cond.sh
+++ cond.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-/* I'm going to write an example for if-else conditionals in bash */
+/*I'm going to write an example for if-else conditionals in bash */
 
-number = $1
+number=$1
 
 if [ $number -lt 10 ]; then
     echo "The number is less than 10"
