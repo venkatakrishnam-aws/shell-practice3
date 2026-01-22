5654297307b0617d7a86fae761a2ebe5e51d42be
venkatakrishnam.aws <vk2860094@gmail.com>
2026-01-22 09:44:37 +0530

    this is my fourth push

--- cond.sh
+++ cond.sh
@@ -2,9 +2,9 @@
 
 number=$1
 
-if [ $number -lt 10 ]; then
+if [ $number -lt 10 ] then
     echo "The number is less than 10"
-elif [ $number -eq 10 ]; then
+elif [ $number -eq 10 ] then
     echo "The number is equal to 10"
 else
     echo "The number is greater than 10"
