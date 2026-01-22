19d3d0e4f255934f7759037f6855af76fb4b5d7c
venkatakrishnam.aws <vk2860094@gmail.com>
2026-01-22 20:04:17 +0530

    This is my latest commit

--- install command.sh
+++ install command.sh
@@ -2,11 +2,11 @@
 
 USERID=$(id -u)
 
-if [ $USERID -eq 0 ]; then
-    echo "You are root."
+if [ $USERID -ne 0 ]; then
+    echo "You are not root user please run the script with root access."
     exit 1
 else
-    echo "You are not root."
+    echo "You are a root user."
 fi
 
 sudo apt install mysql-server
