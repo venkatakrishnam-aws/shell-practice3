4affa379a6fa815952e62d849382d088540a44d1
venkatakrishnam.aws <vk2860094@gmail.com>
2026-01-22 19:58:04 +0530

    This is my second push

--- install command.sh
+++ install command.sh
@@ -4,11 +4,11 @@ USERID=$(id -u)
 
 if [ $USERID -eq 0 ]; then
     echo "You are root."
+    exit 1
 else
     echo "You are not root."
 fi
 
 sudo apt install mysql-server
-systemctl start mysqld
-systemctl enable mysqld
+
 
