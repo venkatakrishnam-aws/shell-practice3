5728dba2408b0508b1bb6f58e3cbf72ae65af2d7
venkatakrishnam.aws <vk2860094@gmail.com>
2026-01-23 20:01:55 +0530

    This is my Third push

--- colors.sh
+++ colors.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 USERID=$(id -u)
-echo -e "\e[32m This is a green colored text.\e[0m"
-echo -e "\e[31m This is a red colored text.\e[0m
-echo -e "\e[34m This is a blue colored text.\e[0m"
+echo -e "\e[32m This is a green colored text"
+echo -e "\e[31m This is a red colored text"
+echo -e "\e[34m This is a blue colored text."
