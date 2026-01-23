722fc78b016e5cd0ee8044551f8d9dd35a15b068
venkatakrishnam.aws <vk2860094@gmail.com>
2026-01-23 08:42:42 +0530

    This is my latest push

--- install command.sh
+++ install command.sh
@@ -6,8 +6,16 @@ if [ $USERID -ne 0 ]; then
     echo "You are not a root user. please run the script with root access."
     exit 1
 else
-    echo "You are a root user."
+    echo "You are a root user hence proceeding with the installation."
 fi
+
+apt list --installed | grep mysql-server &> /dev/null
+ if [ $? -eq 0 ]; then
+    echo "MySQL server is already installed."
+    exit 0
+    else
+    echo "MySQL server is not installed. Proceeding with installation."
+ fi
 
 sudo apt install mysql-server
 
