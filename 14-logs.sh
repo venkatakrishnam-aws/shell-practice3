c61595d0ae834f54ea4b552c6e74f47a4d5dc371
venkatakrishnam.aws <vk2860094@gmail.com>
2026-01-30 19:10:36 +0530

    This is my third push

--- 14-logs.sh
+++ 14-logs.sh
@@ -5,30 +5,30 @@ logs_folder="/var/log/shellscript-logs"    /* Corrected folder name to match scr
 script_name=$(echo $0 | cut -d '.' -f1)    /* Corrected to use $0 for script name */
 log_file="$logs_folder/$script_name.log"   /* Corrected date format */
 mkdir -p $logs_folder
-echo "script started at $(date)" : &>> $log_file
+echo "script started at $(date)" : | tee -a $log_file
 if [ $USERID -ne 0 ]; then
-    echo "You are not a root user. please run the script with root access."  &>> $log_file
+    echo "You are not a root user. please run the script with root access."  | tee -a $log_file
     exit 1
 else
-    echo "You are a root user hence proceeding with the installation."   &>> $log_file
+    echo "You are a root user hence proceeding with the installation."   | tee -a $log_file
 fi  
 validate_installation() {
     if [ $1 -ne 0 ]; then
-        echo "$2 failed to install."  &>> $log_file
+        echo "$2 failed to install."  | tee -a $log_file
         exit 1
     else
-        echo "$2 installed successfully."  &>> $log_file
+        echo "$2 installed successfully."  | tee -a $log_file
     fi
 }
 
 install_package() {
     PACKAGE=$1
     if ! dpkg -l | grep -q "^ii  $PACKAGE "; then
-        echo "$PACKAGE was not installed. Proceeding with installation..."  &>> $log_file
+        echo "$PACKAGE was not installed. Proceeding with installation..."  | tee -a $log_file
         apt install -y $PACKAGE
         validate_installation $? "$PACKAGE"
     else
-        echo "$PACKAGE is already installed."  &>> $log_file
+        echo "$PACKAGE is already installed."  | tee -a $log_file
     fi
 }           
 
@@ -38,7 +38,7 @@ install_package mysql-server
 install_package php
 install_package curl
 
-echo "script ended at $(date)" : &>> $log_file
+echo "script ended at $(date)" : &>> | tee -a $log_file $log_file $log_file
 
 
 
